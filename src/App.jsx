1a6967fcaa0dbe2405a6846e2a4596269c961b37
Hardip Jandu <hardip_jandu.com>
2024-04-26 18:59:03 +0100

    changed app.jsx hashrouter to browserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/home/home'
 import About from './pages/about/about'
 import Contact from './pages/contact/contact'
